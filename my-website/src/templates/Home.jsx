a8d7706d32aa0c45811bd302d0ce0115bb5cf0e8
Sadhu <adwityasadhu@gmail.com>
2023-08-23 15:11:09 +0700

    Temporary Finalized
    To Do, add more projects

--- my-website/src/templates/Home.jsx
+++ my-website/src/templates/Home.jsx
@@ -12,13 +12,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import WorksSlider from "../components/slider/WorksSlider";
-import {WORKS,sliderSettings} from "../constant";
+import { WORKS, sliderSettings } from "../constant";
 
 function Home() {
   useEffect(() => {
     AOS.init();
   }, []);
-
 
   return (
     <div className="home">
@@ -54,22 +53,24 @@ function Home() {
         id="about"
       >
         <div className="md:container md:mx-auto">
-          <div className="flex md:mx-auto items-center justify-between gap-x-8">
+          <div className="flex items-center justify-around gap-x-8">
             <div className="introduction" data-aos="fade-left">
               <h1 className="text-white font-medium text-l md:text-5xl pb-8">
                 About Me
               </h1>
-              <p className="text-[#F8F7FF]">
-                I'm currently a student at Universitas Jember on Information
+              <p className="text-[#F8F7FF] ">
+                I'm a Fresh Graduates from Universitas Jember on Information
                 System's Bachelor Degree Program 💻
                 <br />
-                You can Call me Dhu. What i do? as far i acknowledge my skill is
-                Web Development especially front-end,
+                You can Call me Dhu. What i do? I do Web Development, with focus
+                on front-end (currently learning backend),
+                <br />
+                i also do data related stuff like Data Analytics and data
+                science. On top of that i also do Machine Learning Engineering
                 <br />
                 Data Science, Mobile Development a litle bit. And some Office
                 like Excel and word
-                <br /> Photograph is one of my hobbies, therefore i'll include
-                my photograph as well 😁.
+                <br /> I also do photograpy as one of my hobbies. <a className="font-bold hover:text-sky-500" href="https://www.eyeem.com/u/34204359">See here</a> if you interested .
               </p>
               <br />
               <p className="text-[#F8F7FF]">
@@ -77,6 +78,23 @@ function Home() {
                 hardworker and capable to learn more!!
                 <br />
               </p>
+
+              <div className="md:container md:mx-auto socialLink pt-12 flex gap-x-3">
+                <a href="https://github.com/nuwbie-11/" target="_blank" className="text-white hover:text-[#e8eaea] hover:scale-125 transition duration-150 ease-out hover:ease-in">
+                    <FontAwesomeIcon
+                    icon={faGithub}
+                    size="lg"
+                    // style={{ color: "white" }}
+                    ></FontAwesomeIcon>
+                </a>
+                <a href="https://www.linkedin.com/in/adwitya-sadhup/" target="_blank" className="text-white hover:text-[#0072b1] hover:scale-125 transition duration-150 ease-out hover:ease-in">
+                    <FontAwesomeIcon
+                    icon={faLinkedin}
+                    size="lg"
+                    // style={{ color: "white" }}
+                    ></FontAwesomeIcon>
+                </a>
+              </div>
             </div>
 
             <div className="pfp mr-8 ">
@@ -92,19 +110,6 @@ function Home() {
               {/* </div> */}
             </div>
           </div>
-
-          <div className="md:container md:mx-auto socialLink pt-12 flex gap-x-3">
-            <FontAwesomeIcon
-              icon={faGithub}
-              size="lg"
-              style={{ color: "white" }}
-            ></FontAwesomeIcon>
-            <FontAwesomeIcon
-              icon={faLinkedin}
-              size="lg"
-              style={{ color: "white" }}
-            ></FontAwesomeIcon>
-          </div>
         </div>
       </div>
 
@@ -115,6 +120,7 @@ function Home() {
               <h1 className="text-center text-white font-medium text-l md:text-5xl mb-8">
                 Works
               </h1>
+              <p className="text-center text-slate-600 italic ">Shows most anticipated projects of mine. More on my github Pages!!</p>
             </div>
             <div
               className="md:mx-auto"
@@ -122,18 +128,17 @@ function Home() {
                 width: "80vw",
               }}
             >
-            <Slider {...sliderSettings}>
-
-              {WORKS &&
-                WORKS.map((item) => (
-                  <WorksSlider
-                    title={item.title}
-                    description={item.description}
-                    url={item.url}
-                    imageUrl={item.imageUrl}
-                  />
-                ))}
-            </Slider>
+              <Slider {...sliderSettings}>
+                {WORKS &&
+                  WORKS.map((item) => (
+                    <WorksSlider
+                      title={item.title}
+                      description={item.description}
+                      url={item.url}
+                      imageUrl={item.imageUrl}
+                    />
+                  ))}
+              </Slider>
             </div>
           </div>
         </div>
